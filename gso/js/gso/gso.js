36085b7c365ad8c474b8f50a62ef2dcbd6e371e4
Joseph Simpson <jjs0sbw@gmail.com>
2016-08-01 19:49:40 -0700

    run standard fix on gso.js

--- gso/js/gso/gso.js
+++ gso/js/gso/gso.js
@@ -1,11 +1,12 @@
+/* global Gso, gso, $, Backbone */
 // Wrap closure in jQuery "ready" function, to ensure that the DOM
 // is fully available for the rest of our application.
 $(function () {
-  'use strict';
+  'use strict'
 
   // Initialize application components.
   // The collection object comes first as views depend on it.
-  gso.collection = new Gso.Collections.Structures();
+  gso.collection = new Gso.Collections.Structures()
 
   // Views come next. Lazy dependency on router internally, meaning
   // that by the time we start using view methods, the `app.router`
@@ -15,20 +16,19 @@ $(function () {
   // called.
   gso.structuresView = new Gso.Views.Structures({
     collection: gso.collection
-  });
-  gso.structureNavView = new Gso.Views.StructureNav();
+  })
+  gso.structureNavView = new Gso.Views.StructureNav()
 
   // Router has dependencies on `app.*View` objects, so comes
   // after.
-  gso.router = new Gso.Routers.Router();
+  gso.router = new Gso.Routers.Router()
 
   // Wait until we have our initial collection from the backing
   // store before firing up the router.
-  gso.collection.once("reset", function () {
-    Backbone.history.start();
-  });
+  gso.collection.once('reset', function () {
+    Backbone.history.start()
+  })
 
   // Now fetch collection data, kicking off everything.
-  gso.collection.fetch({ reset: true });
-
-});
+  gso.collection.fetch({ reset: true })
+})
