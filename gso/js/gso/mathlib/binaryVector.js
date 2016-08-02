de3db30123b9cd35032f155d7a6007242202daa9
Joseph Simpson <jjs0sbw@gmail.com>
2016-08-01 20:24:12 -0700

    fixing more js format

--- gso/js/gso/mathlib/binaryVector.js
+++ gso/js/gso/mathlib/binaryVector.js
@@ -1,81 +1,72 @@
+/*global Gso */
 (function () {
-  'use strict';
+  'use strict'
+  Gso.MathLib.BinaryVector = function BinaryVector () {
+    this.setElements = function (newElements) {
+      this.elements = (newElements.elements || newElements).slice()
 
-  Gso.MathLib.BinaryVector = function BinaryVector(){
-
-    this.setElements = function(newElements) {
-      this.elements = (newElements.elements || newElements).slice();
-
-      return this;
-    },
-
-     this.setElement = function(position, value) {
+      return this
+    }
+    this.setElement = function (position, value) {
       /* Think about adding a value check 0 or 1 only */
-      if(position < 1|| position > this.elements.length) {
+      if (position < 1 || position > this.elements.length) {
       /* eslint-disable */
       console.log('Element out of range, please enter valid element.');
       alert('Element out of range, please enter valid element.');
       /* eslint-enable */
       } else {
-        this.elements[position-1] = value;
+        this.elements[position - 1] = value
       }
-    },
+    }
 
-    this.element = function(position) {
-      return (position < 1 || position > this.elements.length) ? null :
-      this.elements[position-1];
-    },
+    this.element = function (position) {
+      return (position < 1 || position > this.elements.length) ? null : this.elements[position - 1]
+    }
 
-    this.indexOf = function(valueWanted) {
-      var index = null;
-      var n = this.elements.length;
+    this.indexOf = function (valueWanted) {
+      var index = null
+      var n = this.elements.length
       for (var i = 0; i < n; i++) {
         /* eslint-disable */
         if (index === null && this.elements[i] == valueWanted) {
         /* eslint-enable */
-          index = i + 1;
+          index = i + 1
         }
       }
 
-      return index;
-    },
-
-    this.view = function() {
-      return '[' + this.elements.join(', ') + ']';
+      return index
     }
 
-
-  },
-
-  Gso.MathLib.BinaryVector.newOne = function(elements) {
-    var bv = new Gso.MathLib.BinaryVector();
-
-    return bv.setElements(elements);
-  },
-
-  Gso.MathLib.BinaryVector.zero = function(size) {
-    var elements = [];
-    var index;
-    for(index = 0; index < size; index++)
-    {
-      elements.push(0);
+    this.view = function () {
+      return '[' + this.elements.join(', ') + ']'
     }
-    var bv = new Gso.MathLib.BinaryVector();
-
-    return bv.setElements(elements);
-  },
-
-  Gso.MathLib.BinaryVector.one = function(size) {
-    var elements = [];
-    var index;
-    for(index = 0; index < size; index++)
-    {
-      elements.push(1);
-    }
-    var bv = new Gso.MathLib.BinaryVector();
-
-    return bv.setElements(elements);
   }
 
+  Gso.MathLib.BinaryVector.newOne = function (elements) {
+    var bv = new Gso.MathLib.BinaryVector()
 
-}());
+    return bv.setElements(elements)
+  }
+
+  Gso.MathLib.BinaryVector.zero = function (size) {
+    var elements = []
+    var index
+    for (index = 0; index < size; index++) {
+      elements.push(0)
+    }
+    var bv = new Gso.MathLib.BinaryVector()
+
+    return bv.setElements(elements)
+  }
+
+  Gso.MathLib.BinaryVector.one = function (size) {
+    var elements = []
+    var index
+    for (index = 0; index < size; index++) {
+      elements.push(1)
+    }
+    var bv = new Gso.MathLib.BinaryVector()
+
+    return bv.setElements(elements)
+  }
+}())
