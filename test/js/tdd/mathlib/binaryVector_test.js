686b3d2c7692810293dfc05d9b0af45f718aa49b
Thomas Kercheval <spacekattpoispin@gmail.com>
2016-08-03 16:06:20 -0700

    Create BinaryVector tests, all failing

---
+++ test/js/tdd/mathlib/binaryVector_test.js
@@ -0,0 +1,36 @@
+/* global suite, expect, Gso, test, assert */
+suite('Gso.MathLib.BinaryVector Factory methods', function () {
+  test('Create new vector', function () {
+    assert(false)
+  })
+
+  test('Create zero vector', function () {
+    assert(false)
+  })
+
+  test('Create vector with all ones', function () {
+    assert(false)
+  })
+})
+
+suite('Gso.MathLib.BinaryVector Operation methods', function () {
+  test('Elements set correctly', function () {
+    assert(false)
+  })
+
+  test('Element set correctly', function () {
+    assert(false)
+  })
+
+  test('Element returned correctly', function () {
+    assert(false)
+  })
+
+  test('Index of element returned correctly', function () {
+    assert(false)
+  })
+
+  test('Vector view valid', function () {
+    assert(false)
+  })
+})
