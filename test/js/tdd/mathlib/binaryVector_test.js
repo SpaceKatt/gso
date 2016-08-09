129ddaec6a6b90b2fe0b0b326a4e74f3b2e5db82
Thomas Kercheval <spacekattpoispin@gmail.com>
2016-08-09 11:43:48 -0700

    Fix BinaryVector tests, all are passing

--- test/js/tdd/mathlib/binaryVector_test.js
+++ test/js/tdd/mathlib/binaryVector_test.js
@@ -22,22 +22,40 @@ suite('Gso.MathLib.BinaryVector Factory methods', function () {
 
 suite('Gso.MathLib.BinaryVector Operation methods', function () {
   test('Elements set correctly', function () {
-    assert(false)
+    var elements = [1, 0, 1, 0, 1]
+    var vector = new Gso.MathLib.BinaryVector()
+    vector.setElements(elements)
+    var expectedElements = [1, 0, 1, 0, 1]
+    assert.deepEqual(vector.elements, expectedElements)
   })
 
   test('Element set correctly', function () {
-    assert(false)
+    var elements = [1, 1, 1, 1, 1]
+    var vector = Gso.MathLib.BinaryVector.newOne(elements)
+    vector.setElement(3, 0)
+    var expectedElement = 0
+    assert.deepEqual(vector.elements[2], expectedElement)
   })
 
   test('Element returned correctly', function () {
-    assert(false)
+    var elements = [0, 0, 1, 0, 0]
+    var vector = Gso.MathLib.BinaryVector.newOne(elements)
+    var returnElem = vector.element(3)
+    var expectedElement = 1
+    assert.deepEqual(returnElem, expectedElement)
   })
 
   test('Index of element returned correctly', function () {
-    assert(false)
+    var elements = [0, 0, 1, 0, 0]
+    var vector = Gso.MathLib.BinaryVector.newOne(elements)
+    var expectedIndex = 3
+    assert.deepEqual(vector.indexOf(1), expectedIndex)
   })
 
   test('Vector view valid', function () {
-    assert(false)
+    var elements = [0, 1, 0, 1]
+    var vector = Gso.MathLib.BinaryVector.newOne(elements)
+    var vectorView = "[0, 1, 0, 1]"
+    assert.deepEqual(vector.view(), vectorView)
   })
 })
